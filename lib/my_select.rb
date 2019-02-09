dafa3b1469bb4fffdd4b7704966ca83e7ffe0da8
Tony Baker <this.tonybaker@gmail.com>
2019-02-09 19:25:42 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+ counter = 0
+ new_array = []
+  while counter < array.length
+    if collection[counter] == 
+      new_array.push(yield (array)[counter])
+      counter += 1
+    end
+  end
 end
